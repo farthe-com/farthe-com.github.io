ec94d1c8b1e41e0df8754e40115479198fc15012
pengshiyu <1940607002@qq.com>
2020-03-22 15:32:07 +0800

    add

--- static/js/data.js
+++ static/js/data.js
@@ -1,14 +1,47 @@
-const list = [
+const tableData = [
     {
-        name: "Iconfont",
-        desc: "",
-        image: "static/img/www.iconfont.cn.png",
-        website: "https://www.iconfont.cn/"
+        name: "前端",
+        icon: "el-icon-monitor",
+        children: [
+            {
+                name: "Iconfont",
+                desc: "阿里妈妈MUX倾力打造的矢量图标管理、交流平台",
+                image: "static/img/www.iconfont.cn.png",
+                website: "https://www.iconfont.cn/"
+            },
+            {
+                name: "Element",
+                desc: "一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库",
+                image: "static/img/element.eleme.cn.png",
+                website: "https://element.eleme.cn/#/zh-CN"
+            },
+
+
+            {
+                name: "Vue.js",
+                desc: "渐进式JavaScript 框架",
+                image: "static/img/cn.vuejs.org.png",
+                website: "https://cn.vuejs.org/"
+            }
+        ]
     },
     {
-        name: "Vue.js",
-        desc: "",
-        image: "static/img/cn.vuejs.org.png",
-        website: "https://cn.vuejs.org/"
+        name: "后台",
+        icon: "el-icon-orange",
+        children: [
+            {
+                name: "AdonisJs",
+                desc: "一个语法清新和优雅的Node.js Web框架",
+                image: "static/img/adonisjs.com.cn.png",
+                website: "https://adonisjs.com.cn/"
+            },
+            {
+                name: "ThinkPHP",
+                desc: "一个为API开发而设计的高性能框架",
+                image: "static/img/www.thinkphp.cn.png",
+                website: "http://www.thinkphp.cn/"
+            }
+        ]
     }
+
 ];
