ee73fe8a00375f4708b3ce289acb6691262cbe74
pengshiyu <1940607002@qq.com>
2020-03-22 20:16:12 +0800

    增加搜索支持

--- static/js/data.js
+++ static/js/data.js
@@ -42,11 +42,29 @@ const tableData = [
                 image: "static/img/adonisjs.com.cn.png",
                 website: "https://adonisjs.com.cn/"
             },
+
+            {
+                name: "Laravel",
+                desc: "一套简洁、优雅的PHP Web开发框架",
+                image: "static/img/laravel.com.ico",
+                website: "https://laravel.com/"
+            },
+
+
             {
                 name: "ThinkPHP",
                 desc: "一个为API开发而设计的高性能框架",
                 image: "static/img/www.thinkphp.cn.png",
                 website: "http://www.thinkphp.cn/"
+            },
+
+            {
+                name: "Masonite",
+                desc: "现代的、以开发人员为中心的Python Web框架",
+                image: "static/img/docs.masoniteproject.com.png",
+                website: "https://docs.masoniteproject.com/"
+
+
             }
         ]
     },
@@ -73,7 +91,30 @@ const tableData = [
                 image: "static/img/shields.io.png",
                 website: "https://shields.io/"
             },
-
+            {
+                name: "菜鸟教程",
+                desc: "提供编程的基础技术教程",
+                image: "static/img/www.runoob.com.png",
+                website: "https://www.runoob.com/"
+            },
+            {
+                name: "BootCDN",
+                desc: "稳定、快速、免费的前端开源项目 CDN 加速服务",
+                image: "static/img/www.bootcdn.cn.png",
+                website: "https://www.bootcdn.cn/"
+            },
+            {
+                name: "CSDN",
+                desc: "专业IT技术社区",
+                image: "static/img/www.csdn.net.ico",
+                website: "https://www.csdn.net/"
+            },
+            {
+                name: "Xclient",
+                desc: "精品MAC应用软件分享",
+                image: "static/img/xclient.info.ico",
+                website: "https://xclient.info/"
+            },
         ]
     },
     {
