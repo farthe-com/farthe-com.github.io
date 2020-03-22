b999137af963afd6e1b4394ad54ed34c26bb52fd
pengshiyu <1940607002@qq.com>
2020-03-22 17:07:30 +0800

    add

--- static/js/data.js
+++ static/js/data.js
@@ -16,6 +16,13 @@ const tableData = [
                 website: "https://element.eleme.cn/#/zh-CN"
             },
 
+            {
+                name: "Vant",
+                desc: "轻量、可靠的移动端 Vue 组件库",
+                image: "static/img/vant.youzan.github.io.png",
+                website: "https://youzan.github.io/vant/#/zh-CN/"
+            },
+
 
             {
                 name: "Vue.js",
@@ -42,6 +49,60 @@ const tableData = [
                 website: "http://www.thinkphp.cn/"
             }
         ]
+    },
+    {
+        name: "工具",
+        icon: "el-icon-suitcase",
+        children: [
+            {
+                name: "JSON",
+                desc: "JSON在线解析及格式化验证",
+                image: "static/img/www.json.cn.png",
+                website: "https://www.json.cn/"
+            },
+            {
+                name: "Github",
+                desc: "面向开源及私有软件项目的托管平台",
+                image: "static/img/github.com.png",
+                website: "https://github.com/"
+            },
+
+            {
+                name: "Shields.io",
+                desc: "Github 上各式各样的小徽章",
+                image: "static/img/shields.io.png",
+                website: "https://shields.io/"
+            },
+
+        ]
+    },
+    {
+        name: "语言",
+        icon: "el-icon-trophy",
+        children: [
+            {
+                name: "Python",
+                desc: "一种跨平台、面向对象的动态类型语言",
+                image: "static/img/www.python.org.png",
+                website: "https://www.python.org/"
+            },
+            {
+                name: "PHP",
+                desc: "一种创建动态交互性站点的强有力的服务器端脚本语言",
+                image: "static/img/www.php.net.png",
+                website: "https://www.php.net/"
+            },
+            {
+                name: "Node.js",
+                desc: "一个基于 Chrome V8 引擎 的 JavaScript 运行时",
+                image: "static/img/nodejs.org.png",
+                website: "https://nodejs.org/zh-cn/"
+            }
+
+
+        ]
     }
+
+
 
 ];
