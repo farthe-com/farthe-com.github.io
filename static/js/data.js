0f20f1a16a494dd13763ed26643d15e1729e84a4
pengshiyu <1940607002@qq.com>
2020-04-18 17:12:41 +0800

    add

--- static/js/data.js
+++ static/js/data.js
@@ -6,29 +6,28 @@ const tableData = [
             {
                 name: "Iconfont",
                 desc: "阿里妈妈MUX倾力打造的矢量图标管理、交流平台",
-                image: "static/img/www.iconfont.cn.ico",
+                image: "www.iconfont.cn.ico",
                 website: "https://www.iconfont.cn/"
             },
-
 
             {
                 name: "Easyicon",
                 desc: "ICON(SVG/PNG/ICO/ICNS)图标搜索下载",
-                image: "static/img/www.easyicon.net.png",
+                image: "www.easyicon.net.png",
                 website: "http://www.easyicon.net/"
             },
 
             {
                 name: "Element",
                 desc: "一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库",
-                image: "static/img/element.eleme.cn.png",
+                image: "element.eleme.cn.png",
                 website: "https://element.eleme.cn/#/zh-CN"
             },
 
             {
                 name: "Vant",
                 desc: "轻量、可靠的移动端 Vue 组件库",
-                image: "static/img/vant.youzan.github.io.png",
+                image: "vant.youzan.github.io.png",
                 website: "https://youzan.github.io/vant/#/zh-CN/"
             },
 
@@ -36,63 +35,69 @@ const tableData = [
             {
                 name: "Vue.js",
                 desc: "渐进式JavaScript 框架",
-                image: "static/img/cn.vuejs.org.png",
+                image: "cn.vuejs.org.png",
                 website: "https://cn.vuejs.org/"
             },
 
             {
                 name: "iView",
                 desc: "一套基于 Vue.js 的高质量UI 组件库",
-                image: "static/img/www.iviewui.com.ico",
+                image: "www.iviewui.com.ico",
                 website: "https://www.iviewui.com/"
             },
             {
                 name: "Ant Design of Vue",
                 desc: "Ant Design 的 Vue 实现，开发和服务于企业级后台产品",
-                image: "static/img/www.antdv.com.ico",
+                image: "www.antdv.com.ico",
                 website: "https://www.antdv.com/"
             },
             {
                 name: "FlatUIColors",
                 desc: "280 handpicked colors",
-                image: "static/img/flatuicolors.com.ico",
+                image: "flatuicolors.com.ico",
                 website: "https://flatuicolors.com/"
             },
             {
                 name: "中国色",
                 desc: "提供各种中国的传统颜色的名称",
-                image: "static/img/zhongguose.com.ico",
+                image: "zhongguose.com.ico",
                 website: "http://zhongguose.com/"
             },
             {
                 name: "Font Awesome",
                 desc: "提供可缩放矢量图标,它可以被定制大小、颜色、阴影以及任何可以用CSS的样式",
-                image: "static/img/www.fontawesome.com.cn.png",
+                image: "www.fontawesome.com.cn.png",
                 website: "http://www.fontawesome.com.cn/"
             },
             {
                 name: "jQuery插件库",
                 desc: "jQuery插件下载",
-                image: "static/img/www.jq22.com.ico",
+                image: "www.jq22.com.ico",
                 website: "http://www.jq22.com/"
             },
             {
                 name: "爱给网",
                 desc: "免费提供免费的音效配乐|3D模型|视频|游戏素材资源下载",
-                image: "static/img/www.aigei.com.ico",
+                image: "www.aigei.com.ico",
                 website: "http://www.aigei.com/"
             },
             {
                 name: "codepen",
                 desc: "The best place to build, test, and discover front-end code.",
-                image: "static/img/codepen.io.ico",
+                image: "codepen.io.ico",
                 website: "https://codepen.io/"
             },
             {
                 name: "uni-app",
                 desc: "使用 Vue.js 开发所有前端应用的框架",
-                image: "static/img/uniapp.dcloud.io.png",
+                image: "uniapp.dcloud.io.png",
                 website: "https://uniapp.dcloud.io/"
+            },
+            {
+                name: "webpack",
+                desc: "打包所有的资源",
+                image: "webpack.js.org.ico",
+                website: "https://webpack.js.org/"
             }
 
         ]
@@ -104,53 +109,53 @@ const tableData = [
             {
                 name: "Laravel",
                 desc: "The PHP Framework for Web Artisans",
-                image: "static/img/laravel.com.ico",
+                image: "laravel.com.ico",
                 website: "https://laravel.com/"
             },
 
             {
                 name: "AdonisJs",
                 desc: "一个语法清新和优雅的Node.js Web框架",
-                image: "static/img/adonisjs.com.cn.png",
+                image: "adonisjs.com.cn.png",
                 website: "https://adonisjs.com.cn/"
             },
 
             {
                 name: "ThinkPHP",
                 desc: "一个为API开发而设计的高性能框架",
-                image: "static/img/www.thinkphp.cn.png",
+                image: "www.thinkphp.cn.png",
                 website: "http://www.thinkphp.cn/"
             },
 
             {
                 name: "Masonite",
                 desc: "现代的、以开发人员为中心的Python Web框架",
-                image: "static/img/docs.masoniteproject.com.png",
+                image: "docs.masoniteproject.com.png",
                 website: "https://docs.masoniteproject.com/"
             },
             {
                 name: "WebMagic",
                 desc: "一个简单灵活的Java爬虫框架",
-                image: "static/img/webmagic.io.ico",
+                image: "webmagic.io.ico",
                 website: "http://webmagic.io/"
             },
             {
                 name: "Django",
                 desc: "Django makes it easier to build better Web apps more quickly and with less code.",
-                image: "static/img/www.djangoproject.com.ico",
+                image: "www.djangoproject.com.ico",
                 website: "https://www.djangoproject.com/"
             },
             {
                 name: "Scrapy",
                 desc: "An open source and collaborative framework for extracting the data you need from websites. In a fast, simple, yet extensible way.",
-                image: "static/img/scrapy.org.png",
+                image: "scrapy.org.png",
                 website: "https://scrapy.org/"
             },
 
             {
                 name: "Nuxt.js",
                 desc: "一个基于 Vue.js 的服务端渲染应用框架",
-                image: "static/img/zh.nuxtjs.org.ico",
+                image: "zh.nuxtjs.org.ico",
                 website: "https://zh.nuxtjs.org/"
             },
 
@@ -163,88 +168,88 @@ const tableData = [
             {
                 name: "JSON",
                 desc: "JSON在线解析及格式化验证",
-                image: "static/img/www.json.cn.png",
+                image: "www.json.cn.png",
                 website: "https://www.json.cn/"
             },
             {
                 name: "Github",
                 desc: "面向开源及私有软件项目的托管平台",
-                image: "static/img/github.com.png",
+                image: "github.com.png",
                 website: "https://github.com/"
             },
 
             {
                 name: "Shields.io",
                 desc: "Github 上各式各样的小徽章",
-                image: "static/img/shields.io.png",
+                image: "shields.io.png",
                 website: "https://shields.io/"
             },
 
             {
                 name: "BootCDN",
                 desc: "稳定、快速、免费的前端开源项目 CDN 加速服务",
-                image: "static/img/www.bootcdn.cn.png",
+                image: "www.bootcdn.cn.png",
                 website: "https://www.bootcdn.cn/"
             },
 
             {
                 name: "Xclient",
                 desc: "精品MAC应用软件分享",
-                image: "static/img/xclient.info.ico",
+                image: "xclient.info.ico",
                 website: "https://xclient.info/"
             },
             {
                 name: "哔哩哔哩",
                 desc: "国内知名的视频弹幕网站",
-                image: "static/img/www.bilibili.com.ico",
+                image: "www.bilibili.com.ico",
                 website: "https://www.bilibili.com/"
             },
 
             {
                 name: "mpv",
                 desc: "a free, open source, and cross-platform media player",
-                image: "static/img/mpv.io.png",
+                image: "mpv.io.png",
                 website: "https://mpv.io/"
             },
 
             {
                 name: "Tampermonkey",
                 desc: "一款免费的浏览器扩展和最为流行的用户脚本管理器",
-                image: "static/img/www.tampermonkey.net.png",
+                image: "www.tampermonkey.net.png",
                 website: "https://www.tampermonkey.net/"
             },
 
             {
                 name: "Typora",
                 desc: "一款轻便简洁的Markdown编辑器,支持即时渲染",
-                image: "static/img/www.typora.io.png",
+                image: "www.typora.io.png",
                 website: "https://www.typora.io/"
             },
 
             {
                 name: "草料二维码",
                 desc: "二维码生成器",
-                image: "static/img/cli.im.ico",
+                image: "cli.im.ico",
                 website: "https://cli.im/"
             },
 
             {
                 name: "Point ML",
                 desc: "Register your FREE .ML Domain Name",
-                image: "static/img/www.point.ml.ico",
+                image: "www.point.ml.ico",
                 website: "http://www.point.ml/"
             },
             {
                 name: "TUNA",
                 desc: "清华大学开源软件镜像站",
-                image: "static/img/mirrors.tuna.tsinghua.edu.cn.png",
+                image: "mirrors.tuna.tsinghua.edu.cn.png",
                 website: "https://mirrors.tuna.tsinghua.edu.cn/"
 
             },
             {
                 name: "EditThisCookie",
                 desc: "A very useful extension, one click to quickly view or remove cookies. ",
-                image: "static/img/www.editthiscookie.com.png",
+                image: "www.editthiscookie.com.png",
                 website: "http://www.editthiscookie.com/"
 
             },
@@ -252,14 +257,14 @@ const tableData = [
             {
                 name: "Cloudflare",
                 desc: "免费CDN网站加速服务",
-                image: "static/img/dash.cloudflare.com.ico",
+                image: "dash.cloudflare.com.ico",
                 website: "https://dash.cloudflare.com/"
 
             },
             {
                 name: "Hexo",
                 desc: "快速、简洁且高效的博客框架",
-                image: "static/img/hexo.io.png",
+                image: "hexo.io.png",
                 website: "https://hexo.io/zh-cn/"
 
             },
@@ -267,7 +272,7 @@ const tableData = [
             {
                 name: "Matplotlib",
                 desc: "Python 的绘图库",
-                image: "static/img/matplotlib.org.ico",
+                image: "matplotlib.org.ico",
                 website: "https://matplotlib.org/"
             },
 
@@ -275,54 +280,54 @@ const tableData = [
             {
                 name: "Heroku",
                 desc: "Cloud Application Platform",
-                image: "static/img/www.heroku.com.ico",
+                image: "www.heroku.com.ico",
                 website: "https://www.heroku.com/"
             },
             {
                 name: "Jinja",
                 desc: "modern and designer-friendly templating language for Python",
-                image: "static/img/jinja.palletsprojects.com.png",
+                image: "jinja.palletsprojects.com.png",
                 website: "https://jinja.palletsprojects.com/"
             },
 
             {
                 name: "lxml",
                 desc: "the most feature-rich and easy-to-use library for processing XML and HTML in the Python language",
-                image: "static/img/lxml.de.ico",
+                image: "lxml.de.ico",
                 website: "https://lxml.de/"
             },
             {
                 name: "CrxDL.COM",
                 desc: "下载Chrome扩展插件Crx离线安装包",
-                image: "static/img/crxdl.com.png",
+                image: "crxdl.com.png",
                 website: "https://crxdl.com/"
 
             },
             {
                 name: "webscraper",
                 desc: "Making web data extraction easy and accessible for everyone",
-                image: "static/img/www.webscraper.io.png",
+                image: "www.webscraper.io.png",
                 website: "https://www.webscraper.io/"
 
             },
             {
                 name: "NPM",
                 desc: " Node package manager",
-                image: "static/img/www.npmjs.com.png",
+                image: "www.npmjs.com.png",
                 website: "https://www.npmjs.com/"
 
             },
             {
                 name: "Foxmail",
                 desc: "邮箱客户端软件",
-                image: "static/img/www.foxmail.com.ico",
+                image: "www.foxmail.com.ico",
                 website: "https://www.foxmail.com/"
             },
 
             {
                 name: "Snip",
                 desc: " 简单高效的截图工具",
-                image: "static/img/snip.qq.com.png",
+                image: "snip.qq.com.png",
                 website: "https://snip.qq.com/"
 
             }
@@ -339,25 +344,25 @@ const tableData = [
             {
                 name: "Python",
                 desc: "一种跨平台、面向对象的动态类型语言",
-                image: "static/img/www.python.org.png",
+                image: "www.python.org.png",
                 website: "https://www.python.org/"
             },
             {
                 name: "PHP",
                 desc: "一种创建动态交互性站点的强有力的服务器端脚本语言",
-                image: "static/img/www.php.net.png",
+                image: "www.php.net.png",
                 website: "https://www.php.net/"
             },
             {
                 name: "Node.js",
                 desc: "一个基于 Chrome V8 引擎 的 JavaScript 运行时",
-                image: "static/img/nodejs.org.png",
+                image: "nodejs.org.png",
                 website: "https://nodejs.org/zh-cn/"
             },
             {
                 name: "Java",
                 desc: "一项用于开发应用程序的技术",
-                image: "static/img/www.java.com.png",
+                image: "www.java.com.png",
                 website: "https://www.java.com/zh_CN/"
             }
 
@@ -374,7 +379,7 @@ const tableData = [
             {
                 name: "知乎",
                 desc: "问答社区",
-                image: "static/img/www.zhihu.com.ico",
+                image: "www.zhihu.com.ico",
                 website: "https://www.zhihu.com/"
 
             },
@@ -382,19 +387,19 @@ const tableData = [
             {
                 name: "掘金",
                 desc: "一个帮助开发者成长的社区",
-                image: "static/img/juejin.im.png",
+                image: "juejin.im.png",
                 website: "https://juejin.im/"
             },
             {
                 name: "CSDN",
                 desc: "专业IT技术社区",
-                image: "static/img/www.csdn.net.ico",
+                image: "www.csdn.net.ico",
                 website: "https://www.csdn.net/"
             },
             {
                 name: "菜鸟教程",
                 desc: "提供编程的基础技术教程",
-                image: "static/img/www.runoob.com.png",
+                image: "www.runoob.com.png",
                 website: "https://www.runoob.com/"
             },
         ]
